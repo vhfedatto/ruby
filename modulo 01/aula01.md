7955120cf8502581aea0d91b8a7434697f5dd17f
Victor Hugo Fedatto <victorhugofedatto@gmail.com>
2025-08-13 13:52:56 -0300

    a.1.0.1

---
+++ modulo 01/aula01.md
@@ -0,0 +1,10 @@
+# Primeiros Passos com Ruby e Fundamentos Universais de Programação
+
+1. O que é Ruby?
+
+- Ruby é uma linguagem de programação interpretada, de alto nível, criada por Yukihiro Matsumoto (Matz) em 1995.
+- É focada na simplicidade e produtividade: código limpo, legível e fácil de escrever.
+- Muito conhecida pelo framework Ruby on Rails, usado para criar sites e APIs.
+
+### 📌 Analogia:
+Ruby é como um carro confortável de dirigir. **Rails** é um carro de corrida feito em cima desse _motor_ — muito rápido, mas com mais peças.
--- modulo 01/conceitos_basicos/03_operadores.rb
+++ modulo 01/conceitos_basicos/03_operadores.rb
@@ -1,3 +1,4 @@
+# 3.1 OPERADORES #
 a = 10
 b = 3
 puts a + b   # soma
@@ -5,3 +6,31 @@ puts a - b   # subtração
 puts a * b   # multiplicação
 puts a / b   # divisão
 puts a % b   # resto
+
+
+# 3.2 BOOLEANOS #
+puts a > b   # true
+puts a == b  # false
+puts a != b  # true
+
+
+# 3.3 COMPARAÇÃO #
+puts a == b   # igual
+puts a != b   # diferente
+puts a > b    # maior que
+puts a < b    # menor que
+puts a >= b   # maior ou igual
+puts a <= b   # menor ou igual
+
+
+# 3.4 OPERADORES LÓGICOS #
+# AND
+condicao1 && condicao2   # true se ambas forem verdadeiras
+condicao1 and condicao2  # mesma coisa, mas com prioridade diferente (usado menos)
+
+# OR
+condicao1 || condicao2   # true se pelo menos uma for verdadeira
+condicao1 or condicao2   # mesmo caso do 'and' (menos usado)
+
+# NOT
+!condicao   # inverte (true → false, false → true)
