a29497fb8bd90b57c8868e99a6e074d9b8daf0a4
Victor Hugo Fedatto <victorhugofedatto@gmail.com>
2025-08-13 13:46:55 -0300

    aula 01.0.0

---
+++ modulo 01/conceitos_basicos/02_entradaSaida.rb
@@ -0,0 +1,17 @@
+puts "Qual o seu nome?"
+nome = gets.chomp
+puts "Olá, #{nome}!"
+
+=begin 
+
+- puts -> imprime e quebra linha 
+- gets -> recebe (input) -- (pega o enter final)
+- chomp -> remove a quebra final (\n)
+- #{} -> insere variáveis dentro de strings (tipo o format do python)
+
+- Para remover a quebra do puts, basta: 
+
+
+
+
+=end
---
+++ modulo 01/conceitos_basicos/03_operadores.rb
@@ -0,0 +1,7 @@
+a = 10
+b = 3
+puts a + b   # soma
+puts a - b   # subtração
+puts a * b   # multiplicação
+puts a / b   # divisão
+puts a % b   # resto
